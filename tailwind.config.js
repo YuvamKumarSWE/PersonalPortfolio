d7f33f849690ecf18624f4c103820fea6fa7091c
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-01-07 17:27:16 -0800

    Landing Page Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
 		fontFamily: {
   			orbitron: ['Orbitron', 'sans-serif'],
 		   	roboto: ['Roboto', 'sans-serif'],
+			noto: ['Noto Znamenny Musical Notation', 'sans-serif'],
 
   		},
 
